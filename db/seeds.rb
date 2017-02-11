d5150e694ac91c829e12ed695712c727ec721ac0
Luke <luke.e.philips@gmail.com>
2017-02-11 13:00:31 -0800

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Tale.create({:title => 'My greatest fear', :picture => 'https://media4.giphy.com/media/7OeXVZZO6Me5O/200_s.gif'});
+Tale.create({:title => "Don't look in there", :picture => 'https://thoughtcatalog.files.wordpress.com/2014/05/shutterstock_127509986.jpg?w=584&h=584'});
+Tale.create({:title => 'And he was never seen again', :picture => 'https://i.ytimg.com/vi/W826KqroeNc/maxresdefault.jpg'});
